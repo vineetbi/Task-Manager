32b6c8359e72210a6f957fd3e6c33a20d0f609e0
Vineet <vineetbindal325@gmail.com>
2022-02-18 18:49:54 +0530

    Add

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ const express=  require('express')
 require('./db/mongoose.js')
 const userRouter= require('./routers/user.js')
 const taskRouter= require('./routers/task.js')
-
+require('dotenv').config({path : '../.env'});
 const app= express()
-const port= process.env.PORT
+console.log(process.env.PORT);
+const port=  3000                  
 
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
 app.listen(port,()=>{
-    console.log('Server is up on port '+port)
+    console.log('Server is up on port '+process.env.PORT)
 })
 
